36e21fcab4f0f2619b7becec76643da774d29702
HikaruEgashira <shighten@gmail.com>
2019-05-28 20:54:13 +0900

    Name

--- locales/ja/index-page-head.js
+++ locales/ja/index-page-head.js
@@ -1,4 +1,4 @@
 export default {
-  title: 'Marina Aísa - Product Designer and Front-End Developer',
-  description: 'I’m a Product Designer (UI/UX) and Front-End Developer based in Barcelona.'
+  title: '江頭輝',
+  description: 'Nuxt勢。いろいろ作ります。<a href="egahika.ml">egahika.ml</a>'
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
     productionUrl
   },
   head: {
-    title: 'Marina Aisa | Product Designer & Front-end Developer',
+    title: 'Hikaru Note',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width,initial-scale=1.0,user-scalable=1' },
